334c7c09624ec21320d94b22ec309f4a3a47d900
Guilherme Matte Embarach <guilherme.embarach@metasa.com.br>
2025-09-04 13:02:07 -0300

    Criado a classificação de IMC

--- ClassificacImc.java
+++ ClassificacImc.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 public class ClassificacImc {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int verifica = 0;
-        String generoIMC = "";
+        String generoInformado = "";
 
         System.out.print("Digite seu nome: ");
         String nome = sc.nextLine();
@@ -14,29 +15,27 @@ public class ClassificacImc {
             System.out.print("Digite M para Masculino, F para Feminino ou N para nb ou não deseja informar:");
             char genero = sc.nextLine().charAt(0);
 
-
             switch (genero) {
                 case 'M', 'm':
                     System.out.println("Masculino");//tirar depois
-                    generoIMC = "M";
+                    generoInformado = "M";
                     verifica++;
                     break;
 
                 case 'F', 'f':
                     System.out.println("Feminino"); //tirar depois
-                    generoIMC = "F";
+                    generoInformado = "F";
                     verifica++;
                     break;
 
                 case 'N', 'n':
                     System.out.println("Não informado"); //tirar depois
-                    generoIMC = "F";
+                    generoInformado = "F";
                     verifica++;
                     break;
 
                 default:
                     System.out.println("Selecione um gênero válido");
-
             }
         }
 
@@ -46,23 +45,35 @@ public class ClassificacImc {
         System.out.print("agora seu peso em KGs: ");
         double peso = sc.nextDouble();
 
-
         double calculaIMC = peso / (altura * altura);
         System.out.println(calculaIMC);
 
-        if (generoIMC.equals("F")) {
+        if (generoInformado.equals("F")) {
             if (calculaIMC <= 19) {
-                System.out.println("IMC abaixo do normal");
-            } else if (calculaIMC > 20 && calculaIMC <= 23.99) {
-                System.out.println("IMC normal");
+                System.out.println("IMC: abaixo do normal");
+            } else if (calculaIMC > 19 && calculaIMC <= 23.99) {
+                System.out.println("IMC: normal");
             } else if (calculaIMC >= 24 && calculaIMC <= 28.99) {
-                System.out.println("IMC obesidade leve");
+                System.out.println("IMC: obesidade leve");
+            } else if (calculaIMC >=29 && calculaIMC <=38.99) {
+                System.out.println("IMC: obesidade moderada");
+            } else if  (calculaIMC >= 39) {
+                System.out.println("IMC: obesidade mórbida");
             }
         }
 
-        if (generoIMC.equals("M") && calculaIMC <= 20) {
-            System.out.println("IMC abaixo do normal");
-
+        if (generoInformado.equals("M")) {
+            if (calculaIMC <= 20) {
+                System.out.println("IMC: abaixo do normal");
+            } else if (calculaIMC > 20 && calculaIMC <= 24.99) {
+                System.out.println("IMC: normal");
+            } else if (calculaIMC >= 25 && calculaIMC <= 29.99) {
+                System.out.println("IMC: obesidade leve");
+            } else if (calculaIMC >=30 && calculaIMC <=39.99) {
+                System.out.println("IMC: obesidade moderada");
+            } else if  (calculaIMC >= 40) {
+                System.out.println("IMC: obesidade mórbida");
+            }
         }
     }
 }
